835cce4e05b1406e28ad577e1c89ae70ef81fe7f
Letjobana <suzanm@gerbertech.co.za>
2021-11-22 08:46:09 +0200

    Added Employee model and DbContext

---
+++ AngularCrud2/AngularCrud2/Data/EmployeeDbContext.cs
@@ -0,0 +1,13 @@
+﻿using AngularCrud2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AngularCrud2.Data
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions<EmployeeDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> GetEmployees { get; set; }
+    }
+}
